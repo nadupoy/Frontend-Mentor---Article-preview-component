cd5e51815142741cec5fb6ac9f4138c1c5d43a02
Nadupoy <gnsampao@gmail.com>
2023-09-22 15:13:15 +0300

    First commit. Pseudo code outlining the the actions required.

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/*
+- In a nutshell, the social media icons become visible when the share icon is clicked.
+- Actions to be performed:
+    1. Display the share icons
+    2. Change the background color of the share icon
+    3. Change the color of the share icon itself
+- The styling & positioning of the share icons is different on mobile & desktop devices.
+*/
