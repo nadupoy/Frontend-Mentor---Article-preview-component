4620af59546278c5355176b2f0cd7dc7accbf614
Nadupoy <gnsampao@gmail.com>
2023-09-23 11:49:11 +0300

    Works as it should.

--- script.js
+++ script.js
@@ -8,3 +8,17 @@
     3. Change the color of the share icon itself
 - The styling & positioning of the share icons is different on mobile & desktop devices.
 */
+
+const shareButton = document.querySelector('button');
+
+shareButton.addEventListener('click', toggleDialog);
+
+function toggleDialog() {
+    const dialogBox = document.querySelector('dialog');
+    
+    if (dialogBox.open == false) {
+        dialogBox.show();
+    } else {
+        dialogBox.close();
+    }
+}
